3463ae0032470be55d5fe56cf72a9c5d60eaf624
Ng Xuan Yee <ngxuanyee@graduate.utm.my>
2025-11-28 01:21:54 +0800

    Create PrimeNumber.cpp

---
+++ PrimeNumber.cpp
@@ -0,0 +1,24 @@
+//Write a program to print prime number from 1-10*
+
+#include <iostream>
+using namespace std;
+int main(){
+  for(int i=1; i<=10; i++){
+    isPrime=true;
+
+    if(i<2){
+      isPrime=false;
+    }
+
+    else{
+      for(int j=2; j*j<=i; j++){
+        if(i%j==0){
+          isPrime=false;
+        }
+      }
+    }
+
+    if(isPrime)
+      cout<<i<<" ";
+  }
+}
